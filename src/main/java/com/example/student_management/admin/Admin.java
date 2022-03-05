14f1e6eb385bb21ec9994d25525f6a69f1bc8cb7
aaronpangan <73965654+aaronpangan@users.noreply.github.com>
2022-03-06 03:56:22 +0800

    add postgresql db

--- src/main/java/com/example/student_management/admin/Admin.java
+++ src/main/java/com/example/student_management/admin/Admin.java
@@ -1,5 +1,76 @@
 package com.example.student_management.admin;
 
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Admin {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    long id;
+
+    String name;
+
+    public Admin(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Admin() {
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Admin id(long id) {
+        setId(id);
+        return this;
+    }
+
+    public Admin name(String name) {
+        setName(name);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Admin)) {
+            return false;
+        }
+        Admin admin = (Admin) o;
+        return id == admin.id && Objects.equals(name, admin.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", name='" + getName() + "'" +
+                "}";
+    }
 }
--- src/main/java/com/example/student_management/configs/SwaggerConfig.java
+++ src/main/java/com/example/student_management/configs/SwaggerConfig.java
@@ -1,5 +1,8 @@
 package com.example.student_management.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 public class SwaggerConfig {
 
 }
